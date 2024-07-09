a0d484024ce9bed192dfbaefdf6b5d0f624ffd16
= <=>
2024-07-10 01:15:56 +0500

    Mongo atlas used.

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const DB = "mongodb://localhost:27017/carselling";
-
+// const DB = "mongodb://localhost:27017/carselling";
+require("dotenv").config();
 async function connectToDatabase() {
   try {
-    await mongoose.connect(DB);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Successfully connected to MongoDB!");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
