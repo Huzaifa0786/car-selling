ffca31c3b21cde3adf2e5d22fbd9ae5451164833
Huzaifa0786 <112539321+Huzaifa0786@users.noreply.github.com>
2024-07-10 05:14:25 +0500

    Update auth.js

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -6,17 +6,17 @@ require("../database/connection");
 const Authenticate = require("../middleware/authenticate");
 
 // Multer setup for file uploads
-app.use("/uploads", express.static("uploads"));
+// app.use("/uploads", express.static("uploads"));
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+// const upload = multer({ storage: storage });
 
 // Login route
 const login = require("../controllers/user");
@@ -24,6 +24,6 @@ router.post("/login", login);
 
 // Add car route
 const { createCar } = require("../controllers/car");
-router.post("/addcar", Authenticate, upload.array("pictures", 10), createCar);
+router.post("/addcar", Authenticate, createCar);
 
 module.exports = router;
