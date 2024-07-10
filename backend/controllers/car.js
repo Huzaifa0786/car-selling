c00104df6875de5337cfe2e4631221402d6c433c
Huzaifa0786 <112539321+Huzaifa0786@users.noreply.github.com>
2024-07-10 05:19:10 +0500

    Update car.js

--- backend/controllers/car.js
+++ backend/controllers/car.js
@@ -1,19 +1,17 @@
 const Car = require("../models/car");
 
 const createCar = async (req, res) => {
-  console.log("outside try")
   try {
-    console.log("into try")
-    const { model, price, phone } = req.body;
-    const picturePaths = req.files.map((file) => `/uploads/${file.filename}`);
+    const { model, price, phone, pictures } = req.body;
+    // const picturePaths = req.files.map((file) => `/uploads/${file.filename}`);
 
     const newCar = new Car({
       model,
       price,
       phone,
-      pictures: picturePaths,
+      pictures: pictures,
     });
-    console.log("before saving in try")
+
     await newCar.save();
     res.status(201).json(newCar);
   } catch (error) {
